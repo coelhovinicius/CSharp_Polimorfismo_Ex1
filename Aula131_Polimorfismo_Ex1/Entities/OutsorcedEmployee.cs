d314d10a151ae9eed1c14d720577574f6c3da62e
coelhovinicius <coelhovinicius@yahoo.com.br>
2021-12-04 17:48:40 -0300

    Polimorfismo_Ex1

---
+++ Aula131_Polimorfismo_Ex1/Entities/OutsorcedEmployee.cs
@@ -0,0 +1,23 @@
+﻿/* >>> CLASSE OUTSORCEDEMPLOYEE <<< */
+namespace Aula131_Polimorfismo_Ex1.Entities
+{
+    class OutsorcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsorcedEmployee()
+        {
+        }
+
+        public OutsorcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base (name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge; // Reaproveita o metodo da Superclasse
+        }
+    }
+}
